38c9dbe096ca4cebca900575c8ec042062cb225e
Ruoqi Wei <wruoqi1006@gmail.com>
2018-12-16 03:07:01 -0500

    Add files via upload

---
+++ HW5/imgReg/imgReg/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace imgReg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInitializeShapes = new System.Windows.Forms.Button();
+            this.panShape1 = new System.Windows.Forms.Panel();
+            this.panShape2 = new System.Windows.Forms.Panel();
+            this.btnApplyTransformation = new System.Windows.Forms.Button();
+            this.panShape3 = new System.Windows.Forms.Panel();
+            this.btnOutlierRemoval = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInitializeShapes
+            // 
+            this.btnInitializeShapes.Location = new System.Drawing.Point(140, 467);
+            this.btnInitializeShapes.Name = "btnInitializeShapes";
+            this.btnInitializeShapes.Size = new System.Drawing.Size(161, 30);
+            this.btnInitializeShapes.TabIndex = 0;
+            this.btnInitializeShapes.Text = "Initialize Shapes";
+            this.btnInitializeShapes.UseVisualStyleBackColor = true;
+            this.btnInitializeShapes.Click += new System.EventHandler(this.btnInitializeShapes_Click);
+            // 
+            // panShape1
+            // 
+            this.panShape1.Location = new System.Drawing.Point(32, 57);
+            this.panShape1.Name = "panShape1";
+            this.panShape1.Size = new System.Drawing.Size(390, 390);
+            this.panShape1.TabIndex = 1;
+            // 
+            // panShape2
+            // 
+            this.panShape2.Location = new System.Drawing.Point(428, 57);
+            this.panShape2.Name = "panShape2";
+            this.panShape2.Size = new System.Drawing.Size(390, 390);
+            this.panShape2.TabIndex = 2;
+            // 
+            // btnApplyTransformation
+            // 
+            this.btnApplyTransformation.Location = new System.Drawing.Point(469, 467);
+            this.btnApplyTransformation.Name = "btnApplyTransformation";
+            this.btnApplyTransformation.Size = new System.Drawing.Size(161, 30);
+            this.btnApplyTransformation.TabIndex = 3;
+            this.btnApplyTransformation.Text = "Apply Transformation";
+            this.btnApplyTransformation.UseVisualStyleBackColor = true;
+            this.btnApplyTransformation.Click += new System.EventHandler(this.btnApplyTransformation_Click);
+            // 
+            // panShape3
+            // 
+            this.panShape3.Location = new System.Drawing.Point(824, 57);
+            this.panShape3.Name = "panShape3";
+            this.panShape3.Size = new System.Drawing.Size(390, 390);
+            this.panShape3.TabIndex = 3;
+            // 
+            // btnOutlierRemoval
+            // 
+            this.btnOutlierRemoval.Location = new System.Drawing.Point(864, 467);
+            this.btnOutlierRemoval.Name = "btnOutlierRemoval";
+            this.btnOutlierRemoval.Size = new System.Drawing.Size(158, 30);
+            this.btnOutlierRemoval.TabIndex = 4;
+            this.btnOutlierRemoval.Text = "Outlier Removal";
+            this.btnOutlierRemoval.UseVisualStyleBackColor = true;
+            this.btnOutlierRemoval.Click += new System.EventHandler(this.btnOutlierRemoval_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1254, 620);
+            this.Controls.Add(this.btnOutlierRemoval);
+            this.Controls.Add(this.panShape3);
+            this.Controls.Add(this.btnApplyTransformation);
+            this.Controls.Add(this.panShape2);
+            this.Controls.Add(this.panShape1);
+            this.Controls.Add(this.btnInitializeShapes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInitializeShapes;
+        private System.Windows.Forms.Panel panShape1;
+        private System.Windows.Forms.Panel panShape2;
+        private System.Windows.Forms.Button btnApplyTransformation;
+        private System.Windows.Forms.Panel panShape3;
+        private System.Windows.Forms.Button btnOutlierRemoval;
+    }
+}
+
---
+++ HW5/imgReg/imgReg/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imgReg
+{
+    public partial class Form1 : Form
+    {
+        List<Point> Shape1 = new List<Point>();
+        List<Point> Shape2 = new List<Point>();
+        List<Point> Shape2Transformed = new List<Point>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInitializeShapes_Click(object sender, EventArgs e)
+        {
+            Shape1.Clear();
+            Shape2.Clear();
+            Point p1a = new Point(20, 30);
+            Point p2a = new Point(120, 50);
+            Point p3a = new Point(160, 80);
+            Point p4a = new Point(180, 300);
+            Point p5a = new Point(100, 220);
+            Point p6a = new Point(50, 280);
+            Point p7a = new Point(20, 140);
+            Shape1.Add(p1a); Shape1.Add(p2a);
+            Shape1.Add(p3a); Shape1.Add(p4a);
+            Shape1.Add(p5a); Shape1.Add(p6a);
+            Shape1.Add(p7a);
+            Transformation T2 = new Transformation();
+            T2.A = 1.05; T2.B = 0.05; T2.T1 = 15; T2.T2 = 22;
+            Shape2 = ApplyTransformation(T2, Shape1);
+            Shape2[2] = new Point(Shape2[2].X + 10, Shape2[2].Y + 3);// change one point
+
+            // add outliers to both shapes
+            Point ptOutlier1 = new Point(200, 230);
+            Shape1.Add(ptOutlier1);
+            Point ptOutLier2 = new Point(270, 160);
+            Shape2.Add(ptOutLier2);
+
+
+            Pen pBlue = new Pen(Brushes.Blue, 1);
+            Pen pRed = new Pen(Brushes.Red, 1);
+            Graphics g = panShape1.CreateGraphics();
+            DisplayShape(Shape1, pBlue, g);
+            DisplayShape(Shape2, pRed, g);
+
+        }
+
+        private void btnApplyTransformation_Click(object sender, EventArgs e)
+        {
+            Transformation T = ICPTransformation.ComputeTransformation(Shape1, Shape2);
+            MessageBox.Show("Cost = " + ICPTransformation.ComputeCost(Shape1, Shape2,T).ToString());
+            List<Point> Shape2T = ApplyTransformation(T, Shape2);
+            Pen pBlue = new Pen(Brushes.Blue, 1);
+            Pen pRed = new Pen(Brushes.Red, 1);
+            Graphics g = panShape2.CreateGraphics();
+            DisplayShape(Shape1, pBlue, g);
+            DisplayShape(Shape2T, pRed, g);
+        }
+
+        void DisplayShape(List<Point> Shp, Pen pen, Graphics g)
+        {
+            Point? prevPoint = null; // nullable
+            foreach (Point pt in Shp)
+            {
+                g.DrawEllipse(pen, new Rectangle(pt.X - 2, pt.Y - 2, 4, 4));
+                if (prevPoint != null)
+                    g.DrawLine(pen, (Point)prevPoint, pt);
+                prevPoint = pt;
+            }
+            g.DrawLine(pen, Shp[0], Shp[Shp.Count - 1]);
+        }
+
+        List<Point> ApplyTransformation(Transformation T, List<Point> shpList)
+        {
+            List<Point> TList = new List<Point>();
+            foreach (Point pt in shpList)
+            {
+                double xprime = T.A * pt.X + T.B * pt.Y + T.T1;
+                double yprime = T.B * pt.X * -1 + T.A * pt.Y + T.T2;
+                Point pTrans = new Point((int)xprime, (int)yprime);
+                TList.Add(pTrans);
+            }
+            return TList;
+        }
+    
+        private void btnOutlierRemoval_Click(object sender, EventArgs e)
+        {
+            Transformation T = ICPTransformation.ComputeTransformation(Shape1, Shape2);
+            double Cost =  ICPTransformation.ComputeCost(Shape1, Shape2, T);
+            List<Point> Shape2T = ApplyTransformation(T, Shape2);
+
+            ComputeModel(Shape1, Shape2T);
+
+            Transformation T2 = ICPTransformation.ComputeTransformation(Shape1, Shape2T);
+            double Cost2 = ICPTransformation.ComputeCost(Shape1, Shape2T, T2);
+            MessageBox.Show("Cost = " + Cost2.ToString());
+            List<Point> Shape2T2 = ApplyTransformation(T2, Shape2T);
+
+            Pen pBlue = new Pen(Brushes.Blue, 1);
+            Pen pRed = new Pen(Brushes.Red, 1);
+            Graphics g = panShape3.CreateGraphics();
+            DisplayShape(Shape1, pBlue, g);
+            DisplayShape(Shape2T2, pRed, g);
+
+        }
+
+        public static double ComputeModel(List<Point> P1List, List<Point> P2List)
+        {
+            double th = 0;
+            double err = 0;
+            int[] best = new int[P1List.Count];
+
+            for (int k = 0; k < 50; k++)
+            {
+                for (int i = 0; i < P1List.Count; i++)
+                {
+                    double distance = Math.Sqrt((P1List[i].X - P2List[i].X) * (P1List[i].X - P2List[i].X) + (P1List[i].Y - P2List[i].Y) * (P1List[i].Y - P2List[i].Y));
+                    th += distance / P1List.Count;
+
+                }
+                for (int i = 0; i < P1List.Count; i++)
+                {
+                    double distance = Math.Sqrt((P1List[i].X - P2List[i].X) * (P1List[i].X - P2List[i].X) + (P1List[i].Y - P2List[i].Y) * (P1List[i].Y - P2List[i].Y));
+                    err = (distance - th) * (distance - th) / P1List.Count;
+                    if (distance > (Math.Sqrt((th * th + th * P1List.Count))) - (th / P1List.Count))
+                         best[i] = 0;
+                    else best[i] = 1;
+
+                }
+                for (int i = 0; i < P1List.Count; i++)
+                {
+                    if (best[i] == 0)
+                    {
+                        P1List.RemoveAt(i);
+                        P2List.RemoveAt(i);
+                    }
+                }
+            }
+
+            return err;
+        }
+
+
+
+
+
+    }
+}
